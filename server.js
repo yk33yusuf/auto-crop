64c2234ff62325fe89ba015e93d92ed7aea081e5
yk33yusuf <yk33yusuf@gmail.com>
2025-12-17 17:30:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const sharp = require('sharp');
 const fs = require('fs').promises;
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
